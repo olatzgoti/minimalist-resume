a2d2b3dd3dca21c9fd6936f2bfad8eb294e8c296
olatzgoti <gotiolatz@gmail.com>
2025-08-12 14:11:58 +0200

    changed loicales default language

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,15 @@ import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://olatzgoti.site',
+  site: 'https://olatzgoti.github.io/minimalist-resume/',
   i18n: {
-    defaultLocale: 'en',
+    defaultLocale: 'es',
     locales: ['en', 'es'],
   },
   integrations: [
     sitemap({
       i18n: {
-        defaultLocale: 'en',
+        defaultLocale: 'es',
         locales: {
           en: 'en',
           es: 'es',
